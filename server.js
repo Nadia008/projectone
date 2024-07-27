1a3334946150c70f22df96d1959632afd8702b2d
Nadia008 <@nikaveronas@mail.ru>
2024-07-27 20:40:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+import { add } from "./math";
+import { subtract } from "./math";
+const math = require('./math')
+console.log("Hello from Parcel!")
+console.log(math.add(5,7));
+console.log(math.subtract(4,3));
